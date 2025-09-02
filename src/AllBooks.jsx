56a45ce8bb5d34a3abfd662547a54e4fd280337d
Gurjeet Singh <abhigurjeet@gmail.com>
2025-09-02 17:13:31 +0530

    fixed images

--- src/AllBooks.jsx
+++ src/AllBooks.jsx
@@ -1,34 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import "./App.css"; // import the CSS file
-const books =[
+import image1 from './assets/image1.png';
+import image2 from './assets/image2.png';
+import image3 from './assets/image3.png';
+
+const books = [
     {
-        id: 1,
-        IsNewReader: false,
-        image: "/src/assets/image1.png",
-        editionName: "Company Law Ready Reckoner",
-        authors: [{ name: "Taxmann's Editorial Board" }],
-        avgRating: 4.3,
-        key:'pdfs/1756810225845_encrypted_pdf1.pdf.enc'
+      id: 1,
+      IsNewReader: false,
+      image: image1,
+      editionName: "Company Law Ready Reckoner",
+      authors: [{ name: "Taxmann's Editorial Board" }],
+      avgRating: 4.3,
+      key: 'pdfs/1756810225845_encrypted_pdf1.pdf.enc'
     },
     {
-        id: 2,
-        IsNewReader: true,
-        image: "/src/assets/image2.png",
-        editionName: "Forensic Audit Decoded – Unlocking the Secrets of Forensic Accounting Investigation",
-        authors: [{ name: "Gyan B. Pipara" }],
-        avgRating: 5,
-        key:'pdfs/1756810469470_encrypted_pdf2.pdf.enc'
+      id: 2,
+      IsNewReader: true,
+      image: image2,
+      editionName: "Forensic Audit Decoded – Unlocking the Secrets of Forensic Accounting Investigation",
+      authors: [{ name: "Gyan B. Pipara" }],
+      avgRating: 5,
+      key: 'pdfs/1756810469470_encrypted_pdf2.pdf.enc'
     },
     {
-        id: 3,
-        IsNewReader: true,
-        image: "/src/assets/image3.png",
-        editionName: "Mutual Fund Foundation",
-        authors: [{ name: "National Institute of Securities Markets | An Educational Initiative of SEBI"}],
-        avgRating: 5,
-        key:'pdfs/1756810480515_encrypted_pdf3.pdf.enc'
+      id: 3,
+      IsNewReader: true,
+      image: image3,
+      editionName: "Mutual Fund Foundation",
+      authors: [{ name: "National Institute of Securities Markets | An Educational Initiative of SEBI" }],
+      avgRating: 5,
+      key: 'pdfs/1756810480515_encrypted_pdf3.pdf.enc'
     }
-]
+  ];
+  
 const AllBooks = () => {
   const navigate = useNavigate();
   const handleNavigation = (key) => {
